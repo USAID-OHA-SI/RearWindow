00cc91b6de1d7cecad8edf1a4e8a7a989a0983e2
Aaron <achafetz@usaid.gov>
2018-09-26 16:09:27 -0400

    setup code to compile dataset for use

---
+++ R/rw_compile.R
@@ -0,0 +1,35 @@
+#' Setup dataset prior 
+#'
+#' @param filepath filepath to the MSD (rds file)
+#' @param opunit operatingunit
+#' @param folderpath_archivedmsd used for creating TX_NET_NEW
+#'
+#' @export
+#' @importFrom dplyr %>%
+
+rw_compile <- function(filepath, opunit, folderpath_archivedmsd = NULL){
+  
+  #import data
+    df <- readr::read_rds(filepath) 
+    
+  #filter ou and select relevant indicators
+    df <- rw_pairdown(df, opunit)
+  
+  #identify current year cum and targets
+    curr_fy <- ICPIutilities::identifypd(df, pd_type = "year")
+    curr_cum <- paste0("fy", curr_fy, "cum")
+    curr_targets <- ICPIutilities::identifypd(df, pd_type = "target")
+    
+  #clean with ICPI utilities - offical names, add net new and have FY18 cum
+    df <- df %>%
+      ICPIutilities::rename_official() %>%
+      ICPIutilities::combine_netnew(folderpath_archivedmsd) %>%
+      ICPIutilities::add_cumulative() 
+  
+  #select key columns
+    df <- df
+      dplyr::select(operatingunit, mechanismid, implementingmechanismname, primepartner, indicator, 
+                    standardizeddisaggregate, agefine, sex, otherdisaggregate, modality, 
+                    curr_cum, curr_targets)
+    
+}
