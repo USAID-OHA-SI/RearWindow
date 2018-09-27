be390f1e40752613b500bff19fb7cb30b79dd1b3
Aaron Chafetz <achafetz@usaid.gov>
2018-09-27 13:28:13 -0400

    save as object

--- R/rw_summarize.R
+++ R/rw_summarize.R
@@ -10,12 +10,11 @@
 rw_summarize <- function(df, ...){
   
   #capture grouping variables as a list of formulas
-  group_vars <- dplyr::quos(...)
+    group_vars <- dplyr::quos(...)
   
   #aggregate up numeric variables based on specificed grouping
-  #df <-
-    df %>% 
-    dplyr::group_by(!!!group_vars) %>%
-    dplyr::summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>%
-    dplyr::ungroup()
+    df <- df %>% 
+      dplyr::group_by(!!!group_vars) %>%
+      dplyr::summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>%
+      dplyr::ungroup()
 }
