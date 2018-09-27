93196f2bc2e330a51c77e5fe777284e4139e019c
Aaron Chafetz <achafetz@usaid.gov>
2018-09-27 16:54:51 -0400

    add function to plot ou/agency achievement

---
+++ R/rw_plot_achv.R
@@ -0,0 +1,28 @@
+#' Plot achievement
+#'
+#' @param df data frame to plot
+#'
+#' @importFrom dplyr %>%
+#' @importFrom ggplot2 aes
+#' @export
+
+rw_plot_achv <- function(df){
+  df %>% 
+    ggplot2::ggplot(aes(reorder(indicator, -achievement), achievement)) + 
+    ggplot2::geom_hline(yintercept = c(thres_low, thres_med, thres_ach), color = c_lgray) +
+    ggplot2::geom_segment(aes(xend=indicator, y=0, yend=achievement), 
+                          color= dplyr::case_when(df$achievement < thres_low  ~ c_ubuntu,
+                                                  df$achievement < thres_med  ~ c_amazon,
+                                                  TRUE                        ~ c_grullo), 
+                          size= 2) +
+    ggplot2::geom_point(color = dplyr::case_when(df$achievement < thres_low  ~ c_ubuntu,
+                                                 df$achievement < thres_med  ~ c_amazon,
+                                                 TRUE                        ~ c_grullo), 
+                        size = 6) +
+    ggplot2::geom_text(aes(label = scales::percent(achievement)), 
+                       hjust=-.5, vjust=.3, color = c_txtgray) +
+    ggplot2::coord_flip() +
+    ggplot2::xlab("") +
+    ggplot2::ylab("") +
+    rw_plot_theme()
+}
