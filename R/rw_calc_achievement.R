3d5dc055b7b7c5e68c8e8cb60ab432155685fa3e
Aaron Chafetz <achafetz@usaid.gov>
2018-09-27 14:54:57 -0400

    setup achievement calculation

---
+++ R/rw_calc_achievement.R
@@ -0,0 +1,21 @@
+#' Calcuate achievement
+#'
+#' @param df dataframe to add achievement onto
+#'
+#' @export
+
+rw_calc_achievement <- function(df){
+  
+  #calculate achievement
+    df_agg <- dplyr::mutate(df, achievement = round(fy2018cum/fy2018_targets, 2))
+  
+  #TODO make calculation dynamic
+    
+  #remove errors (eg denom = 0 -> Inf)
+    df_agg <- dplyr::mutate(df_agg, achievement = ifelse(is.finite(achievement), achievement, NA))
+  
+  #order ascending
+    df_agg <- arrange(df_agg, achievement)
+    
+  return(df_agg)
+}
