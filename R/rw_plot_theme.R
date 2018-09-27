c44dfb81777e0d92f46a5e88410c520ad9cd2282
Aaron Chafetz <achafetz@usaid.gov>
2018-09-27 16:49:22 -0400

    clean up and make explicit

--- R/rw_plot_theme.R
+++ R/rw_plot_theme.R
@@ -4,14 +4,14 @@
 #'
 
 rw_plot_theme <- function(){
-  theme_light() %+replace%
-    theme(
-      legend.position="none",
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      panel.grid.major.y = element_blank(),
-      panel.border = element_blank(),
-      axis.text.x=element_blank(),
-      axis.ticks.x = element_blank(),
-      axis.ticks.y = element_blank())
+  ggplot2::theme_light() +
+  ggplot2::theme(
+      legend.position    = "none",
+      panel.grid.major   = ggplot2::element_blank(),
+      panel.grid.minor   = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_blank(),
+      panel.border       = ggplot2::element_blank(),
+      axis.text.x        = ggplot2::element_blank(),
+      axis.ticks.x       = ggplot2::element_blank(),
+      axis.ticks.y       = ggplot2::element_blank())
 }
