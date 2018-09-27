2f1b5376c65d3e7cf4c82f913d14c95422d31877
Aaron Chafetz <achafetz@usaid.gov>
2018-09-27 11:51:48 -0400

    add summarize function

---
+++ R/rw_summarize.R
@@ -0,0 +1,21 @@
+#' Summarize numeric data
+#'
+#' @param df data frame to aggregate
+#' @param ... list of variables to group collapse/aggregate on
+#'
+#' @export
+#' @importFrom dplyr %>%
+
+
+rw_summarize <- function(df, ...){
+  
+  #capture grouping variables as a list of formulas
+  group_vars <- dplyr::quos(...)
+  
+  #aggregate up numeric variables based on specificed grouping
+  #df <-
+    df %>% 
+    dplyr::group_by(!!!group_vars) %>%
+    dplyr::summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>%
+    dplyr::ungroup()
+}
