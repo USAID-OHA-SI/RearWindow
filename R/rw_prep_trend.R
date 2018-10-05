6e3e610a4034ad06aebced1ccfb2078babcc694b
Aaron Chafetz <achafetz@usaid.gov>
2018-10-05 16:23:29 -0400

    function to prep for trend plotting

---
+++ R/rw_prep_trend.R
@@ -0,0 +1,29 @@
+
+#' Reshape selected mechanism/indicator data long
+#'
+#' @param df data frame to use
+#' @param ind indicator selection
+#' @param mechid mechanism to filter to
+#'
+#' @importFrom dplyr %>%
+#' @export
+
+rw_prep_trend <- function(df, ind, mechid){
+  
+  df %>%
+    dplyr::filter(indicator %in% ind,
+                  standardizeddisaggregate == "Total Numerator",
+                  mechanismid == mechid) %>%
+    dplyr::group_by(mechanismid, indicator) %>%
+    dplyr::summarise_at(dplyr::vars(dplyr::contains("q"), dplyr::ends_with("targets")), 
+                        ~ sum(., na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    tidyr::gather(pd, val, -mechanismid, -indicator) %>%
+    # spread(indicator, val) %>%
+    tidyr::separate(pd, c("fy", "qtr"), 6, remove = FALSE) %>% 
+    dplyr::mutate(fy = stringr::str_remove(fy, "20") %>% toupper(),
+           qtr = stringr::str_remove(qtr, "_") %>% toupper(),
+           qtr_num = ifelse(qtr!="TARGETS", stringr::str_remove(qtr, "Q"), 0)) %>% 
+    dplyr::filter(qtr_num <= ICPIutilities::identifypd(df, "quarter")) %>% 
+    dplyr::select(-qtr_num)
+}
