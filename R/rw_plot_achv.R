9df33adae684a7744a1b0586ee30eb69f3e9a455
Aaron Chafetz <achafetz@usaid.gov>
2018-10-10 12:12:03 -0400

    add rw_prep_achieve in and adjust df names for clarity

--- R/rw_plot_achv.R
+++ R/rw_plot_achv.R
@@ -7,16 +7,20 @@
 #' @export
 
 rw_plot_achv <- function(df){
-  df %>% 
+  
+  #setup table to graph
+  ach <- rw_prep_achv(df)
+  
+  ach %>% 
     ggplot2::ggplot(aes(reorder(indicator, -achievement), achievement)) + 
     ggplot2::geom_hline(yintercept = c(thres_low, thres_med, thres_ach), color = c_lgray) +
     ggplot2::geom_segment(aes(xend=indicator, y=0, yend=achievement), 
-                          color= dplyr::case_when(df$achievement < thres_low  ~ c_ubuntu,
-                                                  df$achievement < thres_med  ~ c_amazon,
+                          color= dplyr::case_when(ach$achievement < thres_low  ~ c_ubuntu,
+                                                  ach$achievement < thres_med  ~ c_amazon,
                                                   TRUE                        ~ c_grullo), 
                           size= 2) +
-    ggplot2::geom_point(color = dplyr::case_when(df$achievement < thres_low  ~ c_ubuntu,
-                                                 df$achievement < thres_med  ~ c_amazon,
+    ggplot2::geom_point(color = dplyr::case_when(ach$achievement < thres_low  ~ c_ubuntu,
+                                                 ach$achievement < thres_med  ~ c_amazon,
                                                  TRUE                        ~ c_grullo), 
                         size = 6) +
     ggplot2::geom_text(aes(label = scales::percent(achievement)), 
