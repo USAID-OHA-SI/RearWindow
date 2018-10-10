01624787f39062213250810d941a1c81604e9e50
Aaron Chafetz <achafetz@usaid.gov>
2018-10-10 14:49:51 -0400

    wrapper to create multiplot for each underperforming IM's indicator

---
+++ R/rw_output_mech.R
@@ -0,0 +1,31 @@
+#' Prepare Multiplot for each partner's underperforming mechanism
+#'
+#' @param df data frame to use
+#' @param ind indicator, eg "HTS_TST"
+#' @param mech implementing mechanism, eg "21212"
+#'
+#' @importFrom dplyr %>%
+#' @export
+
+rw_output_mech <- function(df, ind, mech){
+  
+  #plot result trends
+    v1 <- rw_plot_trend(df, ind, mech)
+    
+  #plot targets
+    v2 <- rw_plot_trend(df, ind, mech, target = TRUE)
+  
+  #if additional slide is needed, eg HTS_TST_POS needs HTS_TST
+    if(stringr::str_detect(ind, "_POS")) {
+      v0 <- rw_plot_trend(df, stringr::str_remove(ind, "_POS"), mech)
+    }
+  
+  #create multiplot with either 2 or 3 columns depending on what's needed
+    if(stringr::str_detect(ind, "_POS")){
+      rw_multiplot(v0, v1, v2, cols = 3)
+    } else {
+      rw_multiplot(v1, v2, cols = 2)
+    }
+  
+}
+  
