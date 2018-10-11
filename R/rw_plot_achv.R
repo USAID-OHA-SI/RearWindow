3e3b46c6658fe035f4b4b2a7afea42be8ccfecbd
Aaron Chafetz <achafetz@usaid.gov>
2018-10-11 15:44:10 -0400

    add thresholds within function

--- R/rw_plot_achv.R
+++ R/rw_plot_achv.R
@@ -8,26 +8,33 @@
 
 rw_plot_achv <- function(df){
   
-  #setup table to graph
-  ach <- rw_prep_achv(df)
+  #establish thresholds
+    qtr <- ICPIutilities::identifypd(df, "quarter")
+    thres_low <- rw_pull_threshold(qtr, "low")
+    thres_med <- rw_pull_threshold(qtr, "med")
+    thres_ach <- 1L
   
-  ach %>% 
-    ggplot2::ggplot(aes(reorder(indicator, -achievement), achievement)) + 
-    ggplot2::geom_hline(yintercept = c(thres_low, thres_med, thres_ach), color = c_lgray) +
-    ggplot2::geom_segment(aes(xend=indicator, y=0, yend=achievement), 
-                          color= dplyr::case_when(ach$achievement < thres_low  ~ c_ubuntu,
-                                                  ach$achievement < thres_med  ~ c_amazon,
-                                                  TRUE                         ~ c_grullo), 
-                          size= 2) +
-    ggplot2::geom_point(color = dplyr::case_when(ach$achievement < thres_low  ~ c_ubuntu,
-                                                 ach$achievement < thres_med  ~ c_amazon,
-                                                 TRUE                         ~ c_grullo), 
-                        size = 6) +
-    ggplot2::geom_text(aes(label = scales::percent(achievement)), 
-                       hjust=-.5, vjust=.3, color = c_txtgray) +
-    ggplot2::coord_flip() +
-    ggplot2::xlab("") +
-    ggplot2::ylab("") +
-    rw_plot_theme() +
-    ggplot2::theme(axis.text.x = ggplot2::element_blank())
+  #setup table to graph
+    ach <- rw_prep_achv(df)
+  
+  #graph achievement
+    ach %>% 
+      ggplot2::ggplot(aes(reorder(indicator, -achievement), achievement)) + 
+      ggplot2::geom_hline(yintercept = c(thres_low, thres_med, thres_ach), color = c_lgray) +
+      ggplot2::geom_segment(aes(xend=indicator, y=0, yend=achievement), 
+                            color= dplyr::case_when(ach$achievement < thres_low  ~ c_ubuntu,
+                                                    ach$achievement < thres_med  ~ c_amazon,
+                                                    TRUE                         ~ c_grullo), 
+                            size= 2) +
+      ggplot2::geom_point(color = dplyr::case_when(ach$achievement < thres_low  ~ c_ubuntu,
+                                                   ach$achievement < thres_med  ~ c_amazon,
+                                                   TRUE                         ~ c_grullo), 
+                          size = 6) +
+      ggplot2::geom_text(aes(label = scales::percent(achievement)), 
+                         hjust=-.5, vjust=.3, color = c_txtgray) +
+      ggplot2::coord_flip() +
+      ggplot2::xlab("") +
+      ggplot2::ylab("") +
+      rw_plot_theme() +
+      ggplot2::theme(axis.text.x = ggplot2::element_blank())
 }
--- R/rw_plot_achv_age.R
+++ R/rw_plot_achv_age.R
@@ -1,27 +1,31 @@
 
 rw_plot_achv_age <- function(df, ind, mechid = NULL){
   
+  #establish thresholds
+    qtr <- ICPIutilities::identifypd(df, "quarter")
+    thres_med <- rw_pull_threshold(qtr, "med")
+  
   #prep table
-  df_age <- rw_prep_achv_age(df, ind, mechid)
+    df_age <- rw_prep_achv_age(df, ind, mechid)
   
   #graph  
-  df_age %>% 
-    ggplot2::ggplot() +
-    ggplot2::geom_col(ggplot2::aes(agesemifine, fy2018_targets),  
-                      width = 0.8,
-                      fill = ifelse(df_age$achievement < thres_med, c_lubuntu, c_lice)) +
-    ggplot2::geom_col(ggplot2::aes(agesemifine, fy2018cum),  
-                      width = 0.4,
-                      fill = ifelse(df_age$achievement < thres_med, c_ubuntu, c_ice)) +
-    ggplot2::coord_flip() +
-    ggplot2::scale_y_continuous(label=scales::comma) +
-    ggplot2::labs(x = "", y = "") +
-    ggplot2::geom_text(ggplot2::aes(agesemifine, label_y, label = scales::percent(achievement)),
-                       hjust=-.5, vjust=.35, color = c_txtgray) +
-    rw_plot_theme() +
-    ggplot2::theme(
-      axis.line.x        = ggplot2::element_blank(),
-      panel.grid.major.x = ggplot2::element_line(c_lgray)
-    )  
+    df_age %>% 
+      ggplot2::ggplot() +
+      ggplot2::geom_col(ggplot2::aes(agesemifine, fy2018_targets),  
+                        width = 0.8,
+                        fill = ifelse(df_age$achievement < thres_med, c_lubuntu, c_lice)) +
+      ggplot2::geom_col(ggplot2::aes(agesemifine, fy2018cum),  
+                        width = 0.4,
+                        fill = ifelse(df_age$achievement < thres_med, c_ubuntu, c_ice)) +
+      ggplot2::coord_flip() +
+      ggplot2::scale_y_continuous(label=scales::comma) +
+      ggplot2::labs(x = "", y = "") +
+      ggplot2::geom_text(ggplot2::aes(agesemifine, label_y, label = scales::percent(achievement)),
+                         hjust=-.5, vjust=.35, color = c_txtgray) +
+      rw_plot_theme() +
+      ggplot2::theme(
+        axis.line.x        = ggplot2::element_blank(),
+        panel.grid.major.x = ggplot2::element_line(c_lgray)
+      )  
 }
 
