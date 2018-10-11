13e8cb81f655d0ff27e9a34e176c6bd50c81c567
Aaron Chafetz <achafetz@usaid.gov>
2018-10-11 14:14:58 -0400

    make summarize dynamic for current fy

--- R/rw_prep_achv.R
+++ R/rw_prep_achv.R
@@ -10,12 +10,17 @@ rw_prep_achv <- function(df, ...){
   
   #capture grouping variables as a list of formulas
     group_vars <- dplyr::quos(...)
-  
+    
+  #identify current year cum and targets
+    curr_fy <- ICPIutilities::identifypd(df, pd_type = "year")
+    curr_cum <- paste0("fy", curr_fy, "cum")
+    curr_targets <- ICPIutilities::identifypd(df, pd_type = "target")
+    
   #generate a table of cumulative results and targets for all indicators
     df %>% 
       dplyr::filter(standardizeddisaggregate == "Total Numerator") %>%
       dplyr::group_by(!!!group_vars) %>%
-      dplyr::summarise_at(dplyr::vars(fy2018cum, fy2018_targets), ~ sum(., na.rm = TRUE)) %>%
+      dplyr::summarise_at(dplyr::vars(curr_cum, curr_targets), ~ sum(., na.rm = TRUE)) %>%
       dplyr::ungroup() %>% 
       #calculate achievement
       rw_calc_achievement() %>% 
