a80e775f2dd650d645199502d34b3f7174354e18
Aaron Chafetz <achafetz@usaid.gov>
2018-10-11 15:12:15 -0400

    make calc_achievement dynamic to year

--- R/rw_calc_achievement.R
+++ R/rw_calc_achievement.R
@@ -1,15 +1,22 @@
 #' Calcuate achievement
 #'
 #' @param df dataframe to add achievement onto
-#'
+#' @param curr_fy current fiscal year, eg 2018L
+#' 
 #' @export
 
-rw_calc_achievement <- function(df){
+rw_calc_achievement <- function(df, curr_fy){
+  
+  #identify current year cum and targets
+    curr_cum <- paste0("fy", curr_fy, "cum")
+    curr_targets <- paste0("fy", curr_fy, "_targets")
+  
+  #setup for calculating achievement with mutate_
+    var_name <- "achievement"
+    fcn <- paste0("round(", curr_cum, "/", curr_targets, ", 2)")
   
   #calculate achievement
-    df_agg <- dplyr::mutate(df, achievement = round(fy2018cum/fy2018_targets, 2))
-  
-  #TODO make calculation dynamic
+    df_agg <- dplyr::mutate_(df, .dots = setNames(fcn, var_name))
     
   #remove errors (eg denom = 0 -> Inf)
     df_agg <- dplyr::mutate(df_agg, achievement = ifelse(is.finite(achievement), achievement, NA))
