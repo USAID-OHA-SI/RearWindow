12d2c0ba9b618859b65451fee19ff82788774e84
Aaron Chafetz <achafetz@usaid.gov>
2018-10-11 14:13:13 -0400

    allow for user specified grouping variable

--- R/rw_prep_achv.R
+++ R/rw_prep_achv.R
@@ -6,12 +6,15 @@
 #' @export
 
 
-rw_prep_achv <- function(df){
+rw_prep_achv <- function(df, ...){
+  
+  #capture grouping variables as a list of formulas
+    group_vars <- dplyr::quos(...)
   
   #generate a table of cumulative results and targets for all indicators
     df %>% 
       dplyr::filter(standardizeddisaggregate == "Total Numerator") %>%
-      dplyr::group_by(indicator) %>%
+      dplyr::group_by(!!!group_vars) %>%
       dplyr::summarise_at(dplyr::vars(fy2018cum, fy2018_targets), ~ sum(., na.rm = TRUE)) %>%
       dplyr::ungroup() %>% 
       #calculate achievement
