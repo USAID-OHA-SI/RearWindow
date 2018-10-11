d080efe4f7c357fd8d2812d07836cba4b76e38f1
Aaron Chafetz <achafetz@usaid.gov>
2018-10-11 14:41:53 -0400

    allow prep_ach for use with non total numerator

--- R/rw_prep_achv.R
+++ R/rw_prep_achv.R
@@ -1,12 +1,14 @@
 #' Create a table for graphing achievement
 #'
 #' @param df dataframe to 
+#' @param ... grouping variable, eg indicator
+#' @param ind can specify if you want to look at a disagg of one indicator, default = NULL
 #'
 #' @importFrom dplyr %>% 
 #' @export
 
 
-rw_prep_achv <- function(df, ...){
+rw_prep_achv <- function(df, ..., ind = NULL){
   
   #capture grouping variables as a list of formulas
     group_vars <- dplyr::quos(...)
@@ -15,10 +17,16 @@ rw_prep_achv <- function(df, ...){
     curr_fy <- ICPIutilities::identifypd(df, pd_type = "year")
     curr_cum <- paste0("fy", curr_fy, "cum")
     curr_targets <- ICPIutilities::identifypd(df, pd_type = "target")
+  
+  #If ind = NULL, comparing all indicators; otherwise, comparinga disagg for one variable
+    if(is.null(ind)){
+      df <- dplyr::filter(df, standardizeddisaggregate == "Total Numerator")
+    } else {
+      df <- dplyr::filter(df, indicator == ind, standardizeddisaggregate != "Total Numerator")
+    }
     
   #generate a table of cumulative results and targets for all indicators
     df %>% 
-      dplyr::filter(standardizeddisaggregate == "Total Numerator") %>%
       dplyr::group_by(!!!group_vars) %>%
       dplyr::summarise_at(dplyr::vars(curr_cum, curr_targets), ~ sum(., na.rm = TRUE)) %>%
       dplyr::ungroup() %>% 
